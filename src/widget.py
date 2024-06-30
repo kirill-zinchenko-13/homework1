0a054aff2997752f33e6125e33cca2fce9c35743
Kirill <kirillzin95@gmail.com>
2024-06-30 15:29:48 +0300

    after work

---
+++ src/widget.py
@@ -0,0 +1,22 @@
+
+from masks import get_mask_card_number, get_mask_account
+
+
+def mask_card_number(card_info: str) -> str:
+    if card_info.startswith("Счет"):
+        return get_mask_account(card_info)
+    else:
+        return get_mask_card_number(card_info)
+
+
+from datetime import datetime
+
+
+def get_data(date_str):
+    """реобразуем строку в объект datetime"""
+    date_obj = datetime.strptime(date_str, '%Y-%m-%dT%H:%M:%S.%f')
+
+    # Форматируем дату в нужный вид
+    formatted_date = date_obj.strftime('%d.%m.%Y')
+
+    return formatted_date
