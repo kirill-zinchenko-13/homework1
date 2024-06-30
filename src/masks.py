01f6ecdc32c47830d49cf1fb000f7f22f106c087
Kirill <kirillzin95@gmail.com>
2024-06-30 15:31:44 +0300

    after work

--- src/masks.py
+++ src/masks.py
@@ -3,12 +3,6 @@ def get_mask_card_number(card_number: str) -> str:
     return f"{card_number[:4]} {card_number[4:6]}** **** {card_number[-4:]}"
 
 
-print(get_mask_card_number(str(7000792289606361)))
-
-
 def get_mask_account(account_number: str) -> str:
     """Функция принимает на вход номер счета и возвращает его маску."""
     return f"**{account_number[-4:]}"
-
-
-print(get_mask_account(str(73654108430135874305)))
