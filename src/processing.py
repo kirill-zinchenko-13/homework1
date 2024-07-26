89d3f75a667e83779163e5f0281726f3cdf5f327
Kirill <kirillzin95@gmail.com>
2024-07-26 22:52:12 +0300

    2nd function is ready

--- src/processing.py
+++ src/processing.py
@@ -7,3 +7,14 @@ def filter_by_state(transactions: list[dict], state: str = "EXECUTED") -> list[d
     :return: Новый список словарей, соответствующий указанному значению 'state'.
     """
     return [transaction for transaction in transactions if transaction.get("state") == state]
+
+
+def sort_by_date(transactions: list[dict], reverse: bool = True) -> list[dict]:
+    """
+    Функция сортирует список словарей по значению ключа 'date'.
+
+    :param transactions: Список словарей с транзакциями.
+    :param reverse: Параметр, указывающий порядок сортировки (по умолчанию True - убывание).
+    :return: Новый список словарей, отсортированный по дате.
+    """
+    return sorted(transactions, key=lambda x: x["date"], reverse=reverse)
